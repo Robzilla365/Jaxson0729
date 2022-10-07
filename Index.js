89a617189fbd8ca85ea7f19b4dcbc3267f9f9be3
Robzilla365 <Rtriche2@gmail.com>
2022-10-06 22:43:41 -0500

    WHOOOOYEAH

--- Index.js
+++ Index.js
@@ -1,1 +1,19 @@
-//Jackie Jack Jackie JAX
+class Main {
+    public static void main(String[] args) {               
+           int num1 = 55;
+           int num2 = 66;
+
+        System.out.println("Before Swapping");
+        System.out.println("Value of num1 is :" + num1);
+        System.out.println("Value of num2 is :" + num2);
+
+        //add both the numbers and assign it to first
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("Before Swapping");
+        System.out.println("Value of num1 is :" + num1);
+        System.out.println("Value of num2 is :" + num2);
+    }
+}
